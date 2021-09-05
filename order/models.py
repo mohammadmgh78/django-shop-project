cd97c054ff964ec49e84820c5109a5c44285995b
mohammadmgh <mohammad.m.ghasemloo@gmail.com>
2021-09-05 13:16:19 +0430

    create models in apps

--- order/models.py
+++ order/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+from customer.models import Customer
+
+
+class Discount(models.Model):
+    discount = models.IntegerField()
+
+    def __str__(self):
+        return self.discount
+
+class Order(models.Model):
+    products = models.JSONField()
+    number_of_products = models.JSONField()
+    user = models.ForeignKey(Customer, on_delete=models.CASCADE)
+
+
+    def __str__(self):
+        return f'order of {self.user}'
+
+
+
+
+
+
+class OrderHistory(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.RESTRICT)
+
+    def __str__(self):
+        return f'{self.order.__str__()}'
--- product/models.py
+++ product/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+
+    class Meta:
+        verbose_name_plural = "Categories"
+
+    def __str__(self):
+        return f'{self.name}'
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=80)
+    price = models.FloatField()
+    description = models.TextField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.name}'
