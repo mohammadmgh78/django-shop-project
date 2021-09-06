b700393eca8b3f0de799f6feacec207451f1d56d
mohammadmgh <mohammad.m.ghasemloo@gmail.com>
2021-09-06 17:45:02 +0430

    add login

--- users/views.py
+++ users/views.py
@@ -1,4 +1,4 @@
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth import get_user_model
 from django.shortcuts import render, redirect
 
@@ -6,18 +6,23 @@ User = get_user_model()
 
 
 def login_user(request):
-    print('hello')
+    print('1')
     if request.method == 'POST':
+        print('2')
         username = request.POST['username']
         password = request.POST['password']
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
-            return redirect('http://127.0.0.1:8000/university/')
+            return redirect('http://127.0.0.1:8000/admin/')
 
-    return render(request, 'users/base.html')
+    return render(request, 'users/login.html')
 
-# def logout_user(request):
-#     logout(request)
-#     print('success')
-#     return render(request, 'users/login.html')
+
+def logout_user(request):
+    logout(request)
+    return render(request, 'users/login.html')
+
+
+def register_user(request):
+    pass
